041c255f44a4f425a303e1f5ae8a444156153f58
sanjeev mishra <sanjusoftware@gmail.com>
2025-02-06 11:28:41 +0200

    implemented search in  transactions

---
+++ src/lib/utils/StatusBadge.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Badge } from 'flowbite-svelte';
+
+	export let status: string;
+</script>
+
+<div class="flex items-center gap-2">
+	{#if status === 'completed'}
+		<Badge color="green">{status}</Badge>
+	{:else if status === 'cancelled'}
+		<Badge color="red">{status}</Badge>
+	{:else if status === 'inreview'}
+		<Badge color="yellow">{status}</Badge>
+	{:else if status === 'inprogress'}
+		<Badge color="purple">{status}</Badge>
+	{/if}
+</div>
--- src/routes/(authenticated)/transactions/+page.svelte
+++ src/routes/(authenticated)/transactions/+page.svelte
@@ -3,29 +3,33 @@
 		Breadcrumb,
 		BreadcrumbItem,
 		Button,
-		Card,
 		Checkbox,
 		Dropdown,
-		Heading,
-		Input,
-		Table,
 		TableBody,
 		TableBodyCell,
 		TableBodyRow,
 		TableHead,
-		TableHeadCell
+		TableHeadCell,
+		TableSearch,
+		DropdownItem,
+		ButtonGroup
 	} from 'flowbite-svelte';
+
 	import {
-		CalendarMonthOutline,
+		PlusOutline,
+		FilterSolid,
+		ChevronLeftOutline,
 		ChevronDownOutline,
 		ChevronRightOutline
 	} from 'flowbite-svelte-icons';
 
-	import LastRange from './LastRange.svelte';
-	import CreditCard from '../../../lib/utils/CreditCard.svelte';
-	import StatusBadge from './StatusBadge.svelte';
+	import { Section } from 'flowbite-svelte-blocks';
+	import CreditCard from '$lib/utils/CreditCard.svelte';
+	import StatusBadge from '$lib/utils/StatusBadge.svelte';
+    import { onMount } from 'svelte';
+	import paginationData from '$lib/data/transactions.json';
 
-	const headers = [
+    const headers = [
 		'Transaction',
 		'Date & Time',
 		'Amount',
@@ -33,98 +37,198 @@
 		'Payment method',
 		'Status'
 	];
-	const data: [string, string, string, string, string, CreditCard['state']][] = [
-		['Payment from Bonnie Green', 'Apr 23 ,2021', '$2300', '0047568936', 'Visa', 'completed'],
-		['Payment refund to #00910', 'Apr 23 ,2021', '-$670', '0078568936', 'Mastercard', 'completed'],
-		['Payment failed from #087651', 'Apr 18 ,2021', '$234', '0088568934', 'Visa', 'cancelled'],
-		['Payment from Lana Byrd', 'Apr 15 ,2021', '$5000', '0018568911', 'Mastercard', 'inprogress'],
-		['Payment from Jese Leos', 'Apr 15 ,2021', '$2300', '0045568939', 'Mastercard', 'completed'],
-		['Refund to THEMESBERG LLC', 'Apr 11 ,2021', '-$560', '0031568935', 'Visa', 'inreview'],
-		['Payment from Lana Lysle', 'Apr 6 ,2021', '$1437', '0023568934', 'Mastercard', 'inreview'],
-		['Payment to Joseph Mcfall', 'Apr 1 ,2021', '$980', '0057568935', 'Visa', 'completed'],
-		['Payment from Alphabet', 'Mar 23 ,2021', '$11,436', '00836143841', 'Visa', 'inprogress'],
-		['Payment from Bonnie Green', 'Mar 23 ,2021', '$560', '0031568935', 'Mastercard', 'completed']
-	];
+    
+	let divClass = 'bg-white dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden';
+	let innerDivClass =
+		'flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4';
+	let searchClass = 'w-full md:w-1/2 relative';
+	let classInput =
+		'text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2  pl-10';
+
+	let searchTerm = '';
+	let currentPosition = 0;
+	const itemsPerPage = 10;
+	const showPage = 5;
+	let totalPages = 0;
+	let pagesToShow: number[] = [];
+	let totalItems = paginationData.length;
+	let startPage: number;
+	let endPage: number;
+
+	const updateDataAndPagination = () => {
+		const currentPageItems = paginationData.slice(currentPosition, currentPosition + itemsPerPage);
+		renderPagination(currentPageItems.length);
+	};
+
+	const loadNextPage = () => {
+		if (currentPosition + itemsPerPage < paginationData.length) {
+			currentPosition += itemsPerPage;
+			updateDataAndPagination();
+		}
+	};
+
+	const loadPreviousPage = () => {
+		if (currentPosition - itemsPerPage >= 0) {
+			currentPosition -= itemsPerPage;
+			updateDataAndPagination();
+		}
+	};
+
+	const renderPagination = (totalItems: number) => {
+		totalPages = Math.ceil(totalItems / itemsPerPage);
+		const currentPage = Math.ceil((currentPosition + 1) / itemsPerPage);
+
+		startPage = currentPage - Math.floor(showPage / 2);
+		startPage = Math.max(1, startPage);
+		endPage = Math.min(startPage + showPage - 1, totalPages);
+
+		pagesToShow = Array.from({ length: endPage - startPage + 1 }, (_, i) => startPage + i);
+	};
+
+	const goToPage = (pageNumber: number) => {
+		currentPosition = (pageNumber - 1) * itemsPerPage;
+		updateDataAndPagination();
+	};
+
+	$: startRange = currentPosition + 1;
+	$: endRange = Math.min(currentPosition + itemsPerPage, totalItems);
+
+	onMount(() => {
+		// Call renderPagination when the component initially mounts
+		renderPagination(paginationData.length);
+	});
+
+	$: currentPageItems = paginationData.slice(currentPosition, currentPosition + itemsPerPage);
+	$: filteredItems = paginationData.filter(
+		(item) => item.name.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1
+	);
 </script>
 
 <Breadcrumb class="mb-5">
 	<BreadcrumbItem home href="/dashboard">Home</BreadcrumbItem>
 	<BreadcrumbItem href="/transactions">Transactions</BreadcrumbItem>
 </Breadcrumb>
-<Card size="xl" class="shadow-sm max-w-none">
-	<div class="items-center justify-between lg:flex">
-		<div class="mb-4 mt-px lg:mb-0">
-			<Heading tag="h3" class="-ml-0.25 mb-2 text-xl font-semibold dark:text-white">
-				Transactions
-			</Heading>
-		</div>
-		<div class="items-center justify-between gap-3 space-y-4 sm:flex sm:space-y-0">
-			<div class="flex items-center">
-				<Button color="alternative" class="w-fit whitespace-nowrap px-4 py-2">
-					Filter by status
-					<ChevronDownOutline size="lg" />
-				</Button>
-				<Dropdown class="w-44 space-y-3 p-3 text-sm" placement="bottom-start">
-					<li>
-						<Checkbox class="accent-primary-600">Completed (56)</Checkbox>
-					</li>
-					<li><Checkbox checked>Cancelled (56)</Checkbox></li>
-					<li>
-						<Checkbox class="accent-primary-600">In progress (56)</Checkbox>
-					</li>
-					<li><Checkbox checked>In review (97)</Checkbox></li>
-				</Dropdown>
-			</div>
-			<div class="flex items-center space-x-4">
-				<Input placeholder="From" class="w-full">
-					<CalendarMonthOutline slot="left" size="md" />
-				</Input>
-				<Input placeholder="To" class="w-full">
-					<CalendarMonthOutline slot="left" size="md" />
-				</Input>
-			</div>
-		</div>
-	</div>
-	<Table
+
+<Section name="advancedTable" classSection="bg-gray-50 dark:bg-gray-900 p-3 sm:p-5">
+	<TableSearch
+		placeholder="Search"
 		hoverable={true}
-		noborder
-		striped
-		class="mt-6 min-w-full divide-y divide-gray-200 dark:divide-gray-600"
+		bind:inputValue={searchTerm}
+		{divClass}
+		{innerDivClass}
+		{searchClass}
+		{classInput}
 	>
-		<TableHead class="bg-gray-50 dark:bg-gray-700">
+		<div
+			slot="header"
+			class="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0"
+		>
+			<Button>
+				<PlusOutline class="h-3.5 w-3.5 mr-2" />Add product
+			</Button>
+			<Button color="alternative">Actions<ChevronDownOutline class="w-3 h-3 ml-2 " /></Button>
+			<Dropdown class="w-44 divide-y divide-gray-100">
+				<DropdownItem>Mass Edit</DropdownItem>
+				<DropdownItem>Delete all</DropdownItem>
+			</Dropdown>
+			<Button color="alternative">Filter<FilterSolid class="w-3 h-3 ml-2 " /></Button>
+			<Dropdown class="w-48 p-3 space-y-2 text-sm">
+				<h6 class="mb-3 text-sm font-medium text-gray-900 dark:text-white">Choose brand</h6>
+				<li>
+					<Checkbox>Apple (56)</Checkbox>
+				</li>
+				<li>
+					<Checkbox>Microsoft (16)</Checkbox>
+				</li>
+				<li>
+					<Checkbox>Razor (49)</Checkbox>
+				</li>
+				<li>
+					<Checkbox>Nikon (12)</Checkbox>
+				</li>
+				<li>
+					<Checkbox>BenQ (74)</Checkbox>
+				</li>
+			</Dropdown>
+		</div>
+		<TableHead>
 			{#each headers as header}
-				<TableHeadCell class="whitespace-nowrap p-4 font-normal">{header}</TableHeadCell>
+				<TableHeadCell padding="px-4 py-3" scope="col">{header}</TableHeadCell>
 			{/each}
 		</TableHead>
-		<TableBody>
-			{#each data as [name, date, amount, reference, method, status]}
-				<TableBodyRow>
-					<TableBodyCell class="px-4 font-normal">{name}</TableBodyCell>
-					<TableBodyCell class="px-4 font-normal text-gray-500 dark:text-gray-400">
-						{date}
-					</TableBodyCell>
-					<TableBodyCell class="px-4">{amount}</TableBodyCell>
-					<TableBodyCell class="px-4 font-normal  text-gray-500 dark:text-gray-400">
-						{reference}
-					</TableBodyCell>
-					<TableBodyCell
-						class="flex items-center gap-2 px-4 font-normal  text-gray-500 dark:text-gray-400"
-					>
-						<CreditCard cardType={method} />
-						<span>••• {reference.slice(reference.length - 4, reference.length)}</span>
-					</TableBodyCell>
-					<TableBodyCell class="px-4 font-normal"><StatusBadge state={status} /></TableBodyCell>
-				</TableBodyRow>
-			{/each}
+		<TableBody class="divide-y">
+			{#if searchTerm !== ''}
+				{#each filteredItems as item}
+					<TableBodyRow>
+						<TableBodyCell class="px-4 font-normal">{item.name}</TableBodyCell>
+						<TableBodyCell class="px-4 font-normal text-gray-500 dark:text-gray-400">
+							{item.date}
+						</TableBodyCell>
+						<TableBodyCell class="px-4">{item.amount}</TableBodyCell>
+						<TableBodyCell class="px-4 font-normal  text-gray-500 dark:text-gray-400">
+							{item.reference}
+						</TableBodyCell>
+						<TableBodyCell
+							class="flex items-center gap-2 px-4 font-normal  text-gray-500 dark:text-gray-400"
+						>
+							<CreditCard cardType={item.method} />
+							<span>
+								••• {item.reference.slice(item.reference.length - 4, item.reference.length)}
+							</span>
+						</TableBodyCell>
+						<TableBodyCell class="px-4 font-normal">
+							<StatusBadge status={item.status} />
+						</TableBodyCell>
+					</TableBodyRow>
+				{/each}
+			{:else}
+				{#each currentPageItems as item}
+					<TableBodyRow>
+						<TableBodyCell class="px-4 font-normal">{item.name}</TableBodyCell>
+						<TableBodyCell class="px-4 font-normal text-gray-500 dark:text-gray-400">
+							{item.date}
+						</TableBodyCell>
+						<TableBodyCell class="px-4">{item.amount}</TableBodyCell>
+						<TableBodyCell class="px-4 font-normal  text-gray-500 dark:text-gray-400">
+							{item.reference}
+						</TableBodyCell>
+						<TableBodyCell
+							class="flex items-center gap-2 px-4 font-normal  text-gray-500 dark:text-gray-400"
+						>
+							<CreditCard cardType={item.method} />
+							<span>
+								••• {item.reference.slice(item.reference.length - 4, item.reference.length)}
+							</span>
+						</TableBodyCell>
+						<TableBodyCell class="px-4 font-normal">
+							<StatusBadge status={item.status} />
+						</TableBodyCell>
+					</TableBodyRow>
+				{/each}
+			{/if}
 		</TableBody>
-	</Table>
-	<div class="-mb-1 flex items-center justify-between pt-3 sm:pt-6">
-		<LastRange />
-		<a
-			href="#top"
-			class="inline-flex items-center rounded-lg p-1 text-xs font-medium uppercase text-primary-700 hover:bg-gray-100 dark:text-primary-500 dark:hover:bg-gray-700 sm:text-sm"
+		<div
+			slot="footer"
+			class="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4"
+			aria-label="Table navigation"
 		>
-			Transactions report <ChevronRightOutline size="lg" />
-		</a>
-	</div>
-</Card>
+			<span class="text-sm font-normal text-gray-500 dark:text-gray-400">
+				Showing
+				<span class="font-semibold text-gray-900 dark:text-white">{startRange}-{endRange}</span>
+				of
+				<span class="font-semibold text-gray-900 dark:text-white">{totalItems}</span>
+			</span>
+			<ButtonGroup>
+				<Button on:click={loadPreviousPage} disabled={currentPosition === 0}>
+					<ChevronLeftOutline size="xs" class="m-1.5" />
+				</Button>
+				{#each pagesToShow as pageNumber}
+					<Button on:click={() => goToPage(pageNumber)}>{pageNumber}</Button>
+				{/each}
+				<Button on:click={loadNextPage} disabled={totalPages === endPage}>
+					<ChevronRightOutline size="xs" class="m-1.5" />
+				</Button>
+			</ButtonGroup>
+		</div>
+	</TableSearch>
+</Section>
