9061f595fa66cf6d446009bbc19036b9bafcee4d
sanjeev mishra <sanjusoftware@gmail.com>
2025-02-23 14:40:42 +0200

    removed env import as its not needed anymore

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,5 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
 import MicrosoftEntraID from "@auth/sveltekit/providers/microsoft-entra-id"
-import { env } from "$env/dynamic/private"
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
   providers: [MicrosoftEntraID],
