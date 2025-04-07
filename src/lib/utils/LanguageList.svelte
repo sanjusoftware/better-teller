d76e181e22ff63ab665d897531cee77d563bac33
sanjeev mishra <sanjusoftware@gmail.com>
2025-04-07 18:27:02 +0300

    add language selection component to navbar

---
+++ src/lib/utils/LanguageList.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { Button, Dropdown, DropdownItem } from "flowbite-svelte";
+
+</script>
+
+<Button
+	id="languages-list"
+	color="none"
+	class="p-2 mr-1 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+>
+	<span class="fi fi-us h-5 w-5 mt-0.5"></span>
+</Button>
+<Dropdown placement="bottom" triggeredBy="#languages-list">
+    <DropdownItem>
+        <a
+            href="/"
+            class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:text-white dark:hover:bg-gray-600"
+            role="menuitem"
+        >
+            <div class="inline-flex items-center">
+                <span class="fi fi-us h-3.5 w-3.5 mr-2"></span>
+                English (US)
+            </div>
+        </a>
+    </DropdownItem>
+    <DropdownItem>
+        <a
+            href="/"
+            class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:text-white dark:hover:bg-gray-600"
+            role="menuitem"
+        >
+            <div class="inline-flex items-center">
+                <span class="fi fi-bg h-3.5 w-3.5 mr-2"></span>
+                Bulgarian
+            </div>
+        </a>
+    </DropdownItem>
+</Dropdown>
--- src/lib/utils/Navbar.svelte
+++ src/lib/utils/Navbar.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import '../../app.css';
 	import { page } from '$app/state';	
+	import Languages from './LanguageList.svelte';
 	import Notifications from './NotificationList.svelte';
 	import AppsMenu from './AppsMenu.svelte';
 	import UserMenu from './UserMenu.svelte';
@@ -63,43 +64,11 @@
 			{/if}
 		</div>
 		<div class="ms-auto flex items-center text-gray-500 dark:text-gray-400 sm:order-2">
-			<Button
- 				id="languages-list"
- 				color="none"
- 				class="p-2 mr-1 text-gray-500 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-700 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
- 			>
- 				<span class="fi fi-us h-5 w-5 mt-0.5"></span>
- 			</Button>
+			<Languages/>
 			<Notifications />
 			<AppsMenu />
 			<DarkMode />
 			<UserMenu name={user?.name ?? ''} avatar={user?.image ?? ''} email={user?.email ?? ''} />
-		</div>		
-		<Dropdown placement="bottom" triggeredBy="#languages-list">
-			<DropdownItem>
-				<a
-					href="/"
-					class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:text-white dark:hover:bg-gray-600"
-					role="menuitem"
-				>
-					<div class="inline-flex items-center">
-						<span class="fi fi-us h-3.5 w-3.5 mr-2"></span>
-						English (US)
-					</div>
-				</a>
-			</DropdownItem>
-			<DropdownItem>
-				<a
-					href="/"
-					class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:text-white dark:hover:bg-gray-600"
-					role="menuitem"
-				>
-					<div class="inline-flex items-center">
-						<span class="fi fi-bg h-3.5 w-3.5 mr-2"></span>
-						Bulgarian
-					</div>
-				</a>
-			</DropdownItem>
-		</Dropdown>
+		</div>				
 	{/if}
 </Navbar>
