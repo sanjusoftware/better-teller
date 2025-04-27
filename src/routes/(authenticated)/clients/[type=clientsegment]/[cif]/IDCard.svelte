94be745fd4c94014689441bd67bd7261d7e5da7c
sanjeev mishra <sanjusoftware@gmail.com>
2025-04-27 17:17:39 +0300

    handling no accounts case

--- src/routes/(authenticated)/clients/[type=clientsegment]/[cif]/IDCard.svelte
+++ src/routes/(authenticated)/clients/[type=clientsegment]/[cif]/IDCard.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
 	import { Button } from 'flowbite-svelte';
-	import {
-		CheckCircleOutline,
-		CloseCircleOutline,
-		RefreshOutline
-	} from 'flowbite-svelte-icons';
+	import { CheckCircleOutline, CloseCircleOutline, RefreshOutline } from 'flowbite-svelte-icons';
 	let { IDForntImage, IDBackImage } = $props();
 
 	let showFront = $state(true);
@@ -14,43 +10,25 @@
 	}
 </script>
 
-<div>
-	<div class="w-full mb-2 border-b">
-		<div class="flex justify-between mb-2 text-xs">
-			<div class="flex items-center">
-				<CheckCircleOutline class="mr-1" size="lg" color="green" />
-				MRZ-OCR
-			</div>
-			<div class="flex items-center">
-				<CheckCircleOutline class="mr-1" size="lg" color="green" />
-				UV Elements
-			</div>
-			<div class="flex items-center">
-				<CloseCircleOutline class="mr-1" size="lg" color="red" />
-				Validated at MoI
-			</div>
+<div class="flex items-center justify-center">
+	<button onclick={toggleCard} class="w-full text-white">
+		<div class="card-inner" class:flipped={!showFront}>
+			{#if showFront}
+				<div class="card-front">
+					<img src={IDForntImage as string} alt="Front of ID card" class="rounded-lg" />
+				</div>
+			{:else}
+				<div class="card-back">
+					<img src={IDBackImage as string} alt="Back of ID card" class="rounded-lg" />
+				</div>
+			{/if}
 		</div>
-	</div>
-	<div class="flex items-center justify-center">
-		<button onclick={toggleCard} class="w-full text-white">
-			<div class="card-inner" class:flipped={!showFront}>
-				{#if showFront}
-					<div class="card-front">
-						<img src={IDForntImage as string} alt="Front of ID card" />
-					</div>
-				{:else}
-					<div class="card-back">
-						<img src={IDBackImage} alt="Back of ID card" />
-					</div>
-				{/if}
-			</div>
-		</button>
-	</div>
-	<div class="flex items-center justify-center mt-4 space-x-2">
-		<Button size="xs" pill outline onclick={toggleCard}>
-			<RefreshOutline  class="mr-2" />Flip card
-		</Button>
-	</div>
+	</button>
+</div>
+<div class="flex items-center justify-center mt-4 space-x-2">
+	<Button size="xs" pill outline onclick={toggleCard}>
+		<RefreshOutline class="mr-2" />Flip card
+	</Button>
 </div>
 
 <style>
--- src/routes/(authenticated)/clients/[type=clientsegment]/[cif]/products/Accounts.svelte
+++ src/routes/(authenticated)/clients/[type=clientsegment]/[cif]/products/Accounts.svelte
@@ -9,92 +9,94 @@
 	import { formatBalance, statusBorderColor } from '$lib/utils/accountHelper';
 	import OwnershipIndicator from '$lib/utils/OwnershipIndicator.svelte';
 	import StatusIndicator from '$lib/utils/StatusIndicator.svelte';
-	import { Card, Tooltip } from 'flowbite-svelte';
+	import { Button, Card, Tooltip } from 'flowbite-svelte';
 	import { ChevronDownOutline } from 'flowbite-svelte-icons';
-	import { clientPath } from '$lib/utils/pathHelper';
 
 	let accounts = $derived(page.data.Accounts);
 </script>
 
 <div class="grid gap-4 xl:grid-cols-1 xl:gap-4 p-2">
-	{#each accounts as account}
-		<Card
-			size="none"
-			class="bg-gray-50 dark:bg-gray-800 shadow-sm"
-			style="border-left: 4px solid {statusBorderColor(account.status)}"
-		>
-			<div class="flex justify-between w-full">
-				<div>
-					<div class="flex items-center space-x-2">
-						<p class="font-bold text-gray-900 dark:text-gray-400">
-							{account.type} account - ({account.currency})
-						</p>
-						<OwnershipIndicator {account} />
-					</div>
-					<p class="text-sm text-gray-600 dark:text-gray-400">{account.iban}</p>
-					<div class="flex items-center space-x-2">
-						<p class="text-sm text-gray-600 dark:text-gray-400">Status:</p>
-						<StatusIndicator status={account.status} />
-					</div>
-				</div>
-				<div class="text-right flex items-center justify-end space-x-2">
+	{#if accounts.length > 0}
+		{#each accounts as account}
+			<Card
+				size="none"
+				class="bg-gray-50 dark:bg-gray-800 shadow-sm"
+				style="border-left: 4px solid {statusBorderColor(account.status)}"
+			>
+				<div class="flex justify-between w-full">
 					<div>
-						<div class="flex items-center space-x-2 justify-end">
-							<p class="text-sm text-gray-400 dark:text-gray-600">current balance:</p>
-							<p class="text-lg font-bold text-gray-900 dark:text-gray-100">
-								{formatBalance('en-US', [account])}
+						<div class="flex items-center space-x-2">
+							<p class="font-bold text-gray-900 dark:text-gray-400">
+								{account.type} account - ({account.currency})
 							</p>
+							<OwnershipIndicator {account} />
 						</div>
-						<div class="flex items-center space-x-2 justify-end">
-							<p class="text-sm text-gray-400 dark:text-gray-600">available balance:</p>
-							<p class="text-sm font-medium text-gray-900 dark:text-gray-100">
-								{formatBalance('en-US', [account])}
-							</p>
+						<p class="text-sm text-gray-600 dark:text-gray-400">{account.iban}</p>
+						<div class="flex items-center space-x-2">
+							<p class="text-sm text-gray-600 dark:text-gray-400">Status:</p>
+							<StatusIndicator status={account.status} />
 						</div>
 					</div>
-					<button onclick={() => (account.showTransactions = !account.showTransactions)}>
-						{#if account.showTransactions}
-							<ChevronDownOutline size="xl" class="text-green-600 dark:text-gray-400" />
-							<Tooltip>Hide transactions</Tooltip>
-						{:else}
-							<ChevronDownOutline size="xl" class="text-green-600 dark:text-gray-400" />
-							<Tooltip>View transactions</Tooltip>
-						{/if}
-					</button>
+					<div class="text-right flex items-center justify-end space-x-2">
+						<div>
+							<div class="flex items-center space-x-2 justify-end">
+								<p class="text-sm text-gray-400 dark:text-gray-600">current balance:</p>
+								<p class="text-lg font-bold text-gray-900 dark:text-gray-100">
+									{formatBalance('en-US', [account])}
+								</p>
+							</div>
+							<div class="flex items-center space-x-2 justify-end">
+								<p class="text-sm text-gray-400 dark:text-gray-600">available balance:</p>
+								<p class="text-sm font-medium text-gray-900 dark:text-gray-100">
+									{formatBalance('en-US', [account])}
+								</p>
+							</div>
+						</div>
+						<button onclick={() => (account.showTransactions = !account.showTransactions)}>
+							{#if account.showTransactions}
+								<ChevronDownOutline size="xl" class="text-green-600 dark:text-gray-400" />
+								<Tooltip>Hide transactions</Tooltip>
+							{:else}
+								<ChevronDownOutline size="xl" class="text-green-600 dark:text-gray-400" />
+								<Tooltip>View transactions</Tooltip>
+							{/if}
+						</button>
+					</div>
 				</div>
+				{#if account.ownership !== 'primary'}
+					<div class="border-t border-gray-200 dark:border-gray-700 mt-4 pt-2">
+						<p class="text-sm text-gray-600 dark:text-gray-400">
+							Primary owner: <a
+								href="/clients/retail/{account.primaryOwnerId}/products"
+								class="font-medium text-gray-900 dark:text-gray-100"
+							>
+								{account.primaryOwnerId}
+							</a>
+						</p>
+					</div>
+				{/if}
+			</Card>
+		{/each}
+		<div class="p-4 rounded shadow-sm">
+			<div class="flex justify-end space-x-2">
+				<p class="text-md font-bold text-gray-400 dark:text-gray-300">Total balance:</p>
+				<p class="text-lg font-bold text-gray-900 dark:text-gray-100">
+					{formatBalance('en-US', accounts)}
+				</p>
 			</div>
-			{#if account.ownership !== 'primary'}
-				<div class="border-t border-gray-200 dark:border-gray-700 mt-4 pt-2">
-					<p class="text-sm text-gray-600 dark:text-gray-400">
-						Primary owner: <a
-							href="/clients/retail/{account.primaryOwnerId}/products"
-							class="font-medium text-gray-900 dark:text-gray-100"
-						>
-							{account.primaryOwnerId}
-						</a>
-					</p>
-				</div>
-			{/if}
-		</Card>
-	{/each}
-	<div class="p-4 rounded shadow-sm">
-		<div class="flex justify-end space-x-2">
-			<p class="text-md font-bold text-gray-400 dark:text-gray-300">Total balance:</p>
-			<p class="text-lg font-bold text-gray-900 dark:text-gray-100">
-				{formatBalance('en-US', accounts)}
-			</p>
+			<div class="flex justify-end space-x-2">
+				<p class="text-md text-gray-400 dark:text-gray-300">Total available balance:</p>
+				<p class="text-lg font-bold text-gray-900 dark:text-gray-100">
+					{formatBalance('en-US', accounts)}
+				</p>
+			</div>
 		</div>
-		<div class="flex justify-end space-x-2">
-			<p class="text-md text-gray-400 dark:text-gray-300">Total available balance:</p>
-			<p class="text-lg font-bold text-gray-900 dark:text-gray-100">
-				{formatBalance('en-US', accounts)}
-			</p>
+	{:else}
+		<div class="p-4 text-center text-gray-500 dark:text-gray-400 space-y-2">
+			<p class="text-sm">Customer has no accounts opened with us.</p>
+			<Button href="/products/casa">
+				Open new CASA account
+			</Button>
 		</div>
-	</div>
+	{/if}
 </div>
-
-{#if accounts.length === 0}
-	<div class="p-4 text-center text-gray-500 dark:text-gray-400">
-		<h1>No accounts opened.</h1>
-	</div>
-{/if}
