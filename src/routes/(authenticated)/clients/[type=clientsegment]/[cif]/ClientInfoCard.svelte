dd6a978889e4da11fc3a3b883f1e8a55958f732d
sanjeev mishra <sanjusoftware@gmail.com>
2025-04-27 19:33:37 +0300

    added dismissable alter on the id card

--- src/routes/(authenticated)/clients/[type=clientsegment]/[cif]/ClientInfoCard.svelte
+++ src/routes/(authenticated)/clients/[type=clientsegment]/[cif]/ClientInfoCard.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
 	import { humanize } from '$lib/utils/strings';
-	import { Card, Tooltip } from 'flowbite-svelte';
-	import { CheckCircleOutline, CloseCircleOutline } from 'flowbite-svelte-icons';
+	import { Alert, Card, Tooltip } from 'flowbite-svelte';
+	import { CheckCircleOutline, CloseCircleOutline, InfoCircleSolid } from 'flowbite-svelte-icons';
 	import IDCard from './IDCard.svelte';
 	let { client } = $props();
-
 </script>
 
-<h2 class="text-md font-semibold text-gray-500 mb-4">Client's Information</h2>
-<Card class="bg-white dark:bg-gray-800 shadow-md rounded-lg p-4">
+<h2 class="text-md font-semibold text-gray-500 mb-2">Client's Information</h2>
+<Card class="bg-white dark:bg-gray-800 shadow-md rounded-lg p-1">
+	<Alert dismissable color="blue" class="mb-2 p-2">
+		<div class="flex items-center gap-3">
+		  <InfoCircleSolid class="w-5 h-5" />
+		  <p class="mt-2 mb-4 text-sm text-gray-900 dark:text-gray-200">The client's information is not verified yet. Please verify the client's information before proceeding with any transactions.</p>
+		</div>
+	  </Alert>
 	<IDCard IDForntImage={client.id_card_front_image} IDBackImage={client.id_card_back_image} />
-	<div class="grid grid-cols-1 gap-4 xl:grid-cols-1 xl:gap-4">
-		<div class="flex flex-col space-y-3">
+	<div class="grid grid-cols-1 gap-2 xl:grid-cols-1 xl:gap-4">
+		<div class="flex flex-col space-y-2">
 			<div>
 				<div class="text-sm text-gray-500 dark:text-gray-400">Name</div>
 				<div class="text-gray-900 dark:text-gray-200 font-semibold">
